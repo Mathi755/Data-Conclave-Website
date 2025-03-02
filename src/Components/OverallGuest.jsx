3f90410067733e91c1ec8bdc6f8a7927da1b824f
Mathi755 <147333217+Mathi755@users.noreply.github.com>
2025-03-02 15:06:39 +0530

    logo

--- src/Components/OverallGuest.jsx
+++ src/Components/OverallGuest.jsx
@@ -2,7 +2,7 @@ import overallImg from "../utils/Img/banner/overall.jpg"
 function OverallGuest() {
   return (
     <div className="flex justify-center items-center mb-2">
-        <img src={overallImg} alt="" width={1000} />
+        <img src={overallImg} alt="" width={500} />
     </div>
   );
 }
